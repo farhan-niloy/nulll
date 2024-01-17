224512f41f6606292d2500f6ec1e59268773e548
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-17 02:51:41 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
     output: 'export',
-    images: {
-        loader: 'custom',
-        loaderFile: './my-loader.ts',
-    },
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
 }
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        unoptimized: true,
+    },
+}
