6f16beaec06762d166412fd424ab7f44ed16bb61
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-06 18:05:51 +0600

    update

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
+import React from 'react'
 import styles from "./featured.module.css";
 
-const Featured = () => {
+const Featured = () =>  {
     return (
         <div className={styles.container}>
             <h1 className={styles.fh1}>who am i?</h1>
             <div className={styles.fp}>
-                <p>
-                    I'm a <strong className={styles.str}>monk</strong>, who's a{' '}
-                    <strong className={styles.str}>philosopher</strong> passionate about Artificial intelligence, embedded systems, and finds{' '}
-                    <strong className={styles.str}>physics</strong> fun. If I'm living, it's because there exist some great{' '}
-                    <strong className={styles.str}>movies</strong> and TV shows that I enjoy. Here I post my <strong>philosophies</strong> and the philosophies I find interesting.
-                </p>
+                <p>i&apos;m a <strong>monk, </strong>who&apos;s a<strong className={styles.str}> philosopher, </strong>  passionate about <strong className={styles.str}>Artificial intelligence, embedded systems,</strong> and finds <strong className={styles.str}>physics</strong> fun. if i&apos;m living, it&apos;s because there exist some great <strong className={styles.str}>movies</strong> and tv shows that I enjoy. here i post my <strong>philosophies</strong> and the philosophies i find interesting.</p>
             </div>
             <div className={styles.buttn}>
-                <button className={styles.butx}><span><a href={"/about-me"}>more about me</a></span></button>
+                <buton className={styles.butx}><span><a href={"/"}>more about me</a></span></buton>
             </div>
+
         </div>
-    );
-};
+    )
+}
 
-export default Featured;
-
+export default Featured
